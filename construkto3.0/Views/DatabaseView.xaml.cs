d7fb0009b5944e06c6824dc527954119654a4562
XEN <12313>
2025-05-23 08:16:20 +0300

    aya

---
+++ construkto3.0/Views/DatabaseView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace construkto3._0.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DatabaseView.xaml
+    /// </summary>
+    public partial class DatabaseView : Window
+    {
+        public DatabaseView()
+        {
+            InitializeComponent();
+        }
+    }
+}
